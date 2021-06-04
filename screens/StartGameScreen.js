c9bc08290b342299d8f1ffa1d3bb686248446bd7
Muhammad Tegar Akmal <eategar@gmail.com>
2021-06-05 01:05:53 +0700

    alert

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,6 +6,7 @@ import {
   Button,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from "react-native";
 import Card from "../components/Card";
 import Input from "../components/input";
@@ -27,7 +28,12 @@ const StartGameScreen = () => {
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
 
-    if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid Number !",
+        "Number has to be an under between 1 and 99",
+        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
+      );
       return;
     }
     setConfirmed(true);
