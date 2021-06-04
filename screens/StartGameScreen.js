407a51a32b175d079d073ffa1ed0161c925d6f88
Muhammad Tegar Akmal <eategar@gmail.com>
2021-06-04 22:31:07 +0700

    Extracting a card component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { View, StyleSheet, Text, TextInput, Button } from "react-native";
+import Card from "../components/Card";
 
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start New Game </Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" onPress={() => {}}></Button>
           <Button title="Confirm"></Button>
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
