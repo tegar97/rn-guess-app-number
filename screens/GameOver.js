79d34be1ceb1fac59e0a4aeb53e6afab6e434bd4
Muhammad Tegar Akmal <eategar@gmail.com>
2021-06-07 12:43:40 +0700

    fix gameoverscreen

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -47,6 +47,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    paddingVertical: 10,
   },
   imageContainer: {
     height: Dimensions.get("window").width * 0.7,
